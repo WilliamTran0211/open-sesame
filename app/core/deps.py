a7b0e89201f99f6ceb46b53f62a985c557e8df62
William Tran <tiencuongtran.0211@gmail.com>
2026-07-14 00:39:41 +0700

    chore: change depends file

---
+++ app/core/deps.py
@@ -0,0 +1,33 @@
+from typing import Annotated, Optional
+
+from fastapi import Depends
+
+from app.core.config import Settings, get_settings
+from app.core.redis import RedisClient
+from app.core.security import JWTHelper, OTPHelper
+
+_redis_client: Optional[RedisClient] = None
+
+
+async def get_redis_client() -> RedisClient:
+    global _redis_client
+    if _redis_client is None:
+        _redis_client = RedisClient()
+        await _redis_client.connect()
+        return _redis_client
+    return _redis_client
+
+
+async def get_jwt_helper() -> JWTHelper:
+    settings = get_settings()
+    return JWTHelper(secret_key=settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+
+async def get_otp_helper() -> OTPHelper:
+    return OTPHelper(length=6)
+
+
+# Aliases
+RedisDep = Annotated[RedisClient, Depends(get_redis_client)]
+JWTDep = Annotated[JWTHelper, Depends(get_jwt_helper)]
+OTPDep = Annotated[OTPHelper, Depends(get_otp_helper)]
--- app/db/deps.py
+++ app/db/deps.py
@@ -1,6 +1,9 @@
 # Global instance
-from typing import AsyncGenerator
+from typing import Annotated, AsyncGenerator
+
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+
 from .session import session_manager
 
 
@@ -8,3 +11,6 @@ from .session import session_manager
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with session_manager.session() as session:
         yield session
+
+
+DBDep = Annotated[AsyncSession, Depends(get_db)]
